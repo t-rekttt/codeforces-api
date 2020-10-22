90c4863a5e5105a8f30de6f21f6c9e6d92f36046
T-Rekt <t.to.the.rekt@gmail.com>
2020-10-22 17:03:12 +0700

    Added functions to use in contest group

--- index.js
+++ index.js
@@ -105,4 +105,143 @@ let getUpcomingContests = async() => {
   return contests;
 }
 
-module.exports = { getPastContests, getUserSubmissions, getUpcomingContests }
+let getGroupContestsList = async(groupName = 'FLVn1Sc504') => {
+  let url = `https://codeforces.com/group/${groupName}/contests`;
+
+  let r = await request.get(url);
+
+  let $ = cheerio.load(r);
+
+  let lines = $('.datatable table tbody tr');
+
+  let contests = lines.map((i, el) => {
+    el = $(el);
+
+    let cells = el.find('td');
+
+    let name = $(cells[0]).children().remove().end().text().trim();
+
+    let contestId = el.attr('data-contestid');
+
+    let startTime = Date.parse($(cells[1]).find('a').children().remove().end().text().trim());
+
+    let length = $(cells[2]).text().trim();
+
+    let url = `https://codeforces.com/group/${groupName}/contest/${contestId}`;
+
+    return {
+      name,
+      contestId,
+      startTime,
+      length,
+      url
+    }
+  }).get();
+
+  contests.shift();
+
+  return contests;
+}
+
+let getProblemsListInGroupContest = async(groupName = 'FLVn1Sc504', contestListId = '274766') => {
+  let url = `https://codeforces.com/group/${groupName}/contest/${contestListId}`;
+
+  let r = await request.get(url);
+
+  let $ = cheerio.load(r);
+
+  let lines = $('.problems tr');
+
+  let problems = lines.map((i, el) => {
+    el = $(el);
+
+    let cells = el.find('td');
+
+    let problem = $(cells[0]).text().trim();
+
+    let name = $(cells[1]).find('a').text().trim();
+
+    let url = 'https://codeforces.com' + $(cells[1]).find('a').attr('href');
+
+    let solvedCounts = parseInt($(cells[3]).find('a').text().trim().replace('x', '')) || 0;
+
+    return {
+      problem,
+      url,
+      name,
+      solvedCounts
+    }
+  }).get();
+
+  problems.shift();
+
+  return problems;
+}
+
+let getSubmissionsInGroupContest = async(groupName = 'FLVn1Sc504', contestId = '274821', problem = 'L', participant) => {
+  let url = `https://codeforces.com/group/${groupName}/contest/${contestId}/status/${problem}`;
+
+  let jar = request.jar();
+
+  let r = await request.get(url, {
+    resolveWithFullResponse: true,
+    jar
+  });
+
+  let $ = cheerio.load(r.body);
+
+  let csrfToken = $('.csrf-token').attr('data-csrf');
+
+  if (participant) {
+    r = await request.post(url, {
+      jar,
+      form: {
+        action: 'setupSubmissionFilter',
+        frameProblemIndex: problem,
+        verdictName: 'anyVerdict',
+        programTypeForInvoker: 'anyProgramTypeForInvoker',
+        comparisonType: 'NOT_USED',
+        judgedTestCount: '',
+        participantSubstring: participant,
+        csrf_token: csrfToken
+      },
+      followAllRedirects: true
+    });
+  } else {
+    r = r.body;
+  }
+
+  let lines = $('table.status-frame-datatable tr:not(.first-row)');
+
+  return lines.map((i, el) => {
+    el = $(el);
+
+    let cells = el.find('td');
+
+    let submissionId = $(cells[0]).text().trim();
+
+    let submissionTime = Date.parse($(cells[1]).text().trim());
+
+    let problemId = $(cells[3]).attr('data-problemid');
+
+    let user = $(cells[2]).text().trim();
+
+    let problemName = $(cells[3]).text().trim();
+
+    let lang = $(cells[4]).text().trim();
+
+    let verdict = $(cells[5]).text().trim();
+
+    return {
+      submissionId,
+      submissionTime,
+      user,
+      problemName,
+      problemId,
+      lang,
+      verdict
+    }
+  }).get();
+}
+
+module.exports = { getPastContests, getUserSubmissions, getUpcomingContests, getSubmissionsInGroupContest, getProblemsListInGroupContest, getGroupContestsList }
